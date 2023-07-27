0eb8ae630e7005d0d456228e62cd8579b799bea8
khsrla9806 <kimhunsope@naver.com>
2023-07-27 15:32:44 +0900

    feat: Spring Security Cors 설정 및 추가 설정 추가

--- src/main/java/com/mini/anuualwork/config/WebSecurityConfig.java
+++ src/main/java/com/mini/anuualwork/config/WebSecurityConfig.java
@@ -13,6 +13,9 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @RequiredArgsConstructor
 @Configuration
@@ -38,13 +41,17 @@ public class WebSecurityConfig {
         };
 
         httpSecurity
-                .csrf().disable() // csrf 기능 비활성화
+                .cors().configurationSource(this.configurationSource()) // Security Cors 설정
+                .and()
+                .csrf().disable() // csrf 기능 비활성화: POSTMAN 사용을 위해서
+                .formLogin().disable() // Form Login 기능 제거 -> UsernamePasswordAuthenticationFilter 비활성화
+                .httpBasic().disable() // Security 기본 로그인 페이지 뜨지 않도록 비활성화
                 .exceptionHandling()
                 .authenticationEntryPoint(jwtAuthenticationEntryPoint)
                 .accessDeniedHandler(jwtAccessDeniedHandler)
                 .and()
                 .sessionManagement()
-                .sessionCreationPolicy(SessionCreationPolicy.STATELESS) // 세션 사용 X : StateLess 설정
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS) // 세션 사용 X : StateLess 설정 => JSessionId 사라짐
                 .and()
                 .authorizeHttpRequests()
                 .antMatchers("/api/admin/**").hasRole("ADMIN")
@@ -56,5 +63,19 @@ public class WebSecurityConfig {
                 .apply(new JwtSecurityConfig(tokenProvider));
 
         return httpSecurity.build();
+    }
+
+    private CorsConfigurationSource configurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.addAllowedHeader("*");
+        configuration.addAllowedMethod("*"); // 모든 Http Method 요청 허용
+        configuration.addAllowedOriginPattern("*"); // TODO: Front-End IP만 허용 (지금은 모든 IP 접근 허용)
+        configuration.setAllowCredentials(true); // 클라이언트에서 모든 쿠키 요청을 허용
+        configuration.addExposedHeader("Authorization"); // 'Authorization' 헤더를 허용 (JWT)
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration); // /** 경로에 위에서 설정한 모든 Cors 설정을 적용
+
+        return source;
     }
 }
